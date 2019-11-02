cb9549ac8b0ed64154b20f19a98f76f067ca1326
Maarten De Paepe <maarten@adimian.com>
2019-11-02 17:05:13 +0100

    mocking out pyglet

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 
@@ -7,3 +7,10 @@ import pytest
 def sprite():
     points = ((0, 0), (0, 100), (100, 0), (100, 100))
     return Mock(points=points)
+
+
+
+@pytest.fixture(scope='session', autouse=True)
+def create_directories(request):
+    with patch('arcade.application.pyglet'):
+        yield
